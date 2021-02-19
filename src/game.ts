c4e905480f93abb1a16036c9f27a861d18614dc5
Jeremy Honl <jerhon@users.noreply.github.com>
2021-02-19 14:56:08 -0600

    Size to entire current browser viewport.

--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,17 @@
-import * as Phaser  from "phaser"
-import { TankScene } from "./tank-scene";
+import * as Phaser from "phaser"
+import {TankScene} from "./tank-scene";
+import ScaleModes = Phaser.Scale.ScaleModes;
+import Center = Phaser.Scale.Center;
 
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: "100%",
+    height: "100%",
     backgroundColor: '#0000FF',
+    scale: {
+        mode: ScaleModes.FIT,
+        autoCenter: Center.CENTER_BOTH
+    },
     physics: {
         default: 'arcade',
         arcade: {
@@ -13,7 +19,7 @@ const config = {
             debug: process.env.NODE_ENV === 'development'
         }
     },
-
+    parent: 'game'
 } as Phaser.Types.Core.GameConfig
 
 
